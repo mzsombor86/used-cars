8aae5c6be59cffb57e12da329a2181974cccf5c6
mzsombor86 <mzsombor@gmail.com>
2021-06-06 21:09:11 +0200

    First commit.

---
+++ UsedCars/src/main/java/hu/mzsombor/usedcars/UsedCarsApplication.java
@@ -0,0 +1,13 @@
+package hu.mzsombor.usedcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedCarsApplication.class, args);
+	}
+
+}
---
+++ UsedCars/src/test/java/hu/mzsombor/usedcars/UsedCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.mzsombor.usedcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
